d42a248107d8ef07cb023e117832a1b266355ac8
GuiregPato <luisguilhermegt3@gmail.com>
2023-04-10 20:07:35 -0300

    -m 'cu';

--- Pratica.py
+++ Pratica.py
@@ -4,5 +4,5 @@ print (1200 + 452)
 peso = input ('Quantos KG voce pesa?')
 idade = input ('Qual a sua idade?')
 nome = input ('Qual seu nome?')
-print('Olá', nome, 'Seja bem-vindo seu velho de', idade, 'anos, que pesa', peso, 'KGs. (e que se ama muito)')
+print('Olá', nome, 'Seja bem-vindo seu velho de', idade, 'anos, que pesa', peso, ' KGs')
 
