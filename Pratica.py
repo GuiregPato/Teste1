1a584a5abf878ab85972958de31367582e484c6d
GuiregPato <luisguilhermegt3@gmail.com>
2023-04-13 21:48:50 -0300

    tabuada

--- Pratica.py
+++ Pratica.py
@@ -36,9 +36,13 @@ print('O dobro de {} é {}' .format(num, (num*2)))
 print('O triplo de {} é {}\nA raiz quadrada de {} é igual {:.2f}'
       .format(num, (num*3), num, num**(1/2)))"""
 
-n1 = float(input('Primeira nota do aluno: '))
+""""n1 = float(input('Primeira nota do aluno: '))
 n2 = float(input('Segunda nota do aluino: '))
 
-print('Media final do aluno {:.1f}'.format((n1+n2) /2))
+print('Media final do aluno {:.1f}'.format((n1+n2) /2))"""
+
+#TABUADA 
+n = int(input('Digite um numero:'))
+print('-----------------------\n{} X 1 = {}\n{} X 2 = {}\n{} X 3 = {}\n{} X 4 = {}\n{} X 5 = {}\n{} X 6 = {}\n{} X 7 = {}\n{} X 8 = {}\n{} X 9 = {}\n{} X 10 = {}\n-----------------------'.format(n, n*1,n,n*2,n,n*3,n,n*4,n,n*5,n,n*6,n,n*7,n,n*8,n,n*9,n,n*10))
 
   
